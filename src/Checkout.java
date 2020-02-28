a529d4fbf941f1d59af263b95cb0af4077930cdd
crumcake <49697371+crumcake@users.noreply.github.com>
2020-02-27 19:16:32 -0500

    Update Checkout.java

--- src/Checkout.java
+++ src/Checkout.java
@@ -1,5 +1,36 @@
+import java.util.Hashtable;
 
-public class Checkout{
+public class Checkout {
+    private Hashtable<String, Double> cartItems;
 
+    Checkout(Hashtable<String, Double> userCartItems){
+        cartItems = userCartItems;
+    }
 
+    public float calcTotal(){
+        float total =0;
+        /*needs a way to access item prices
+         */
+        for(String key: cartItems.keySet()){
+            //total += cartItems.get(key).getPrice();
+            System.out.println("Key:" + key);
+            System.out.println("Item: " + cartItems.get(key));
+        }
+        return total;
+    }
+    
+    public float taxCalc(Float userSalesTax){
+        float taxTotal = 0;
+        
+        for(String key: cartItems.keySet()){
+            //taxTotal += (cartItems.get(key).getPrice())/userSalesTax;
+        }
+        return taxTotal;
+    }
+    
+    /*Order Confirmation?    */
+    public String toString(){
+        return "Congrats on your purchase";
+    }
 }
+
