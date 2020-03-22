c38d116091afa81142c3d0f747d275c60115b814
Amran2 <60632248+Amran2@users.noreply.github.com>
2020-03-22 11:56:27 -0400

    Update Validations.java

--- src/Validations.java
+++ src/Validations.java
@@ -2,7 +2,9 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+/**
+ * @ author Praygee Nepal
+ */
 public class Validations {
     private String address;
     private String city;
