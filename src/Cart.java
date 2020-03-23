9fcc917ae6b6b89f2e42db770755fae395480ac2
Amran2 <60632248+Amran2@users.noreply.github.com>
2020-03-23 16:19:14 -0400

    Adding Clara's Changes

--- src/Cart.java
+++ src/Cart.java
@@ -1,16 +1,25 @@
+import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.Hashtable;
+ 
+/**
+ * @ author Amran Hassan
+ */
 
 public class Cart {
 
-    private CartDao cartData = new CartDao("hassana", "1810531");
-    private Hashtable<String, Double> items;
-    private Double totalPrice;
+    private static DecimalFormat df = new DecimalFormat("#.##");
 
-    private StateSalesTaxDao taxData = new StateSalesTaxDao("hassana", "1810531");
+    private CartDao cartData = new CartDao("carletoc", "1683864");
+    private double totalPrice;
+
+    private StateSalesTaxDao taxData = new StateSalesTaxDao("carletoc", "1683864");
     private Double taxRate;
     private Double finalPrice;
+    private ArrayList<Integer> item_ids;
 
-    Cart(int userId){}
+    Cart(int id){
+    }
 
 
     public void add(int userId, int prodId){
@@ -19,29 +28,36 @@ public class Cart {
 
     public void remove(int userId, int prodId){cartData.deleteItem(userId, prodId); }
 
-    /*public Hashtable<String, Double> getItems(int userId){
-        items = cartData.items(userId);
-        return items;
-   } */
     public void price(int userId) {cartData.itemPrice(userId);}
 
     public Double getCartPrice(int userId) {
-        totalPrice = cartData.itemPrice(userId);
-        return totalPrice;
+        return cartData.itemPrice(userId);
     }
 
     public Double getSalesTax(int userId) {
-        taxRate= taxData.salesTax(userId);
-        return taxRate;
+        return taxData.salesTax(userId);
     }
 
     public Double getFinalPrice(int userId) {
-        finalPrice = totalPrice + (taxRate*totalPrice);
-        return finalPrice;
+        return getCartPrice(userId) + (getSalesTax(userId)*getCartPrice(userId));
     }
 
     public void list(int userId) {cartData.list(userId);}
-    public void checkout(){
 
+    public ArrayList<Integer> getItemIds(int user_id) {return item_ids = cartData.getItemIds(user_id);}
+
+    public void checkout(int user_id){
+        User user = new User(user_id);
+        Validations v = new Validations(user_id);
+        System.out.println("*****"+user.getName() + " is now checking out"+"*****");
+        if(v.validateAll()){
+            System.out.println("Hello, " + user.getName() + ",\n\t Thank you for shopping with us! Your order " +
+                    "for $" + df.format(getFinalPrice(user_id)) + " has gone through. \nYour purchases are on their way to " +
+                    user.getAddress() + ". A receipt has been sent to " + user.getEmail());
+            list(user_id);
+            cartData.clearCart(user_id);
+        }else{
+            System.out.println(user.getName() + ", please update your information");
+        }
     }
 }
