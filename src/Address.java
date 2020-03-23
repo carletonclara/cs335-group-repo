0d6352420216446211d2ca78177419d83ebe70dc
Amran2 <60632248+Amran2@users.noreply.github.com>
2020-03-23 16:44:10 -0400

    Adding Clara's Changes

---
+++ src/Address.java
@@ -0,0 +1,32 @@
+/**
+ * @ author Clara Carleton
+ */
+
+public class Address {
+    private AddressDao addressData = new AddressDao("carletoc", "1683864");
+    private Address address;
+    private String street;
+    private String city;
+    private String state;
+    private CharSequence zipcode;
+
+
+    Address(int user_id){
+        street = addressData.getAddress(user_id).get(0);
+        city = addressData.getAddress(user_id).get(1);
+        state = addressData.getAddress(user_id).get(2);
+        zipcode = addressData.getAddress(user_id).get(3);
+    }
+
+    public String getStreet(){ return street; }
+
+    public String getCity() { return city; }
+
+    public String getState() { return state; }
+
+    public CharSequence getZipcode() { return zipcode; }
+
+    public String toString(){
+        return street + " " + city + ", " + state + " " + zipcode;
+    }
+}
